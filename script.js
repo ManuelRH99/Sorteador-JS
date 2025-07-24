439025f5992db952e4d4f58050f2ceb0575c86df
ManuelRH99 <manuelrojash90@gmail.com>
2025-07-24 16:25:56 -0300

    Sorteador - Editado

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
+const results = document.querySelector(".results")
+
 function drawingNumber(){
 
     const min = Math.ceil(document.querySelector(".input-min").value) 
     const max = Math.floor( document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;    // faz a conta
+    
+
 
     if (min > max) {
-        alert(result)
+        results.innerHTML = result
+
     }
     else {
-
-        alert("O primer valor tem que ser maior que o segundo")
+        alert("Nao pode ter numero maior")
+       
     }
+
+     
     
 }
+
+
+
 
 
